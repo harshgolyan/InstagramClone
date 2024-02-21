5a75cf81a9c95561000421eee5916c5f286ce7d3
Harsh Golyan <harshgolyan308@gmail.com>
2024-02-21 18:58:56 +0530

    show all posts functionality

--- frontend/src/components/mainscreen/mainscreen.jsx
+++ frontend/src/components/mainscreen/mainscreen.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Postpage from "../posts/postpage";
 import LeftNavbar from "./leftnavbar";
 import TopNavbar from "./topnavbar";
+import ShowAllPost from "../posts/showallpost";
 
 const MainScreen = () =>{
     return<>
@@ -12,7 +13,7 @@ const MainScreen = () =>{
     </div>
     <div className="grid grid-cols-12">
         <div className="grid col-span-3"><LeftNavbar/></div>
-        <div className="grid col-span-6"><Postpage/></div>
+        <div className="grid col-span-6"><ShowAllPost/></div>
         <div className="grid col-span-3"></div>
     </div>
     </>
--- frontend/src/components/posts/postpage.jsx
+++ frontend/src/components/posts/postpage.jsx
@@ -7,15 +7,15 @@ const Postpage = ({postInfo}) =>{
     return<>
     <div className="flex flex-col bg-grey-600 p-3 border-slate-950">
         <div className="p-3 rounded-md mx-auto ">
-            <img src="https://plus.unsplash.com/premium_photo-1674582743901-7e438e0e5ea0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D"
+            <img src={postInfo.photo}
             className="rounded-md"/>
             <div className="flex flex-row mt-2">
             <HeartIcon className="h-8 w-8 text-black-500 m-2" />
             <ChatIcon className="h-8 w-8 text-black-500 m-2" />
             <ShareIcon className="h-8 w-8 text-black-500 m-2" />
         </div>
-            <h1>title :</h1>
-            <p>body of title :</p>
+            <h1>title :{postInfo.title}</h1>
+            <p>body of title :{postInfo.body}</p>
         </div>
             
 
