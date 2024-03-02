462e845e7fd50910f32a252c35338cc089e8b082
Harsh Golyan <harshgolyan308@gmail.com>
2024-03-02 23:12:49 +0530

    user profile updated

--- frontend/src/components/profile/userprofile.jsx
+++ frontend/src/components/profile/userprofile.jsx
@@ -10,6 +10,13 @@ const UserProfile = () =>{
     const [follower,setFollower] = useState({ user: { followers: [] } })
     const navigate = useNavigate()
 
+    const numPost = post.length ? post.length : 0
+
+    const followers = user.followers ? user.followers.length : 0
+    const followercount = follower ?.user?.followers?.length || followers
+
+    const following = user.following ? user.following.length : 0
+
     //id fetching from navigate
     const location = useLocation()
     const id = location.state ? location.state.id : ''
@@ -62,6 +69,7 @@ const UserProfile = () =>{
             setIsFollowed(false)
         }
     }
+
     
     return<>
     <div className="grid grid-cols-12">
@@ -72,26 +80,34 @@ const UserProfile = () =>{
     <div className="grid grid-cols-12">
         <div className="grid col-span-3"><LeftNavbar/></div>
         <div className="grid col-span-6">
-            <div className="grid grid-flow-col">
-                <img 
-                className="h-20 w-20 rounded-full"
-                src="https://images.unsplash.com/photo-1682687221323-6ce2dbc803ab?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8"/>
-                    <div className="grid grid-flow-col">
-                        <div className="text-black text-2xl">{user.name}</div>
-                        {(!isfollowed)?<button 
-                        className="bg-blue-500 p-4 pt-1 pb-1 rounded-md h-10 w-40 text-white"
-                        onClick={follow}
-                        >Follow</button>:
-                        <button 
-                        className="bg-slate-300 p-4 pt-1 pb-1 rounded-md h-10 w-40 text-white"
-                        onClick={follow}
-                        >Follow</button>}
-                        <button className="bg-slate-500 p-4 pt-1 pb-1 rounded-md h-10 w-40 text-white">Message</button>
-                    </div>
-            </div>     
+            <div className="grid grid-flow-row">
+                <div className="grid grid-flow-col">
+                    <img 
+                    className="h-20 w-20 rounded-full"
+                    src="https://images.unsplash.com/photo-1682687221323-6ce2dbc803ab?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8"/>
+                            <div className="text-black text-2xl">{user.name}</div>
+                            <button 
+                            className="bg-blue-500 p-4 pt-1 pb-1 rounded-md h-10 w-40 text-white"
+                            onClick={follow}
+                            >{isfollowed ? 'Following' : 'Follow'}</button>
+                            
+                            <button className="bg-slate-500 p-4 pt-1 pb-1 rounded-md h-10 w-40 text-white">Message</button>
+                </div>
+                <div className="grid grid-flow-col">
+                    <div>{numPost} posts</div>
+                    <div>{followercount} follower</div>
+                    <div>{following} following</div>
+                </div>
+            </div>   
         </div>
+        <div>{post.map((item,idx)=>{
+            <img src={item.photo} key={idx}/>
+        })}</div>
         <div className="grid col-span-3"></div>
     </div>
+    {post.map((item,idx)=>{
+            <img src={item.photo} key={idx}/>
+        })}
     </>
 }
 
